906853227901cc41c44cdb0736cc9bb4e48dc8b9
DuckS0ul <qq13732291063@gmail.com>
2023-10-06 17:35:21 +0800

    Step 3 Finish

--- Assets/input/Scripts/PlayerController.cs
+++ Assets/input/Scripts/PlayerController.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour{
 
     public Vector2 moveValue;
     public float speed;
+    private int count = 0;
+    private int numPickups = 5;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
+
+    void Start()
+    {
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
 
     void OnMove(InputValue value){
         moveValue = value.Get<Vector2>();
@@ -17,5 +30,24 @@ public class PlayerController : MonoBehaviour{
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if(count >= numPickups)
+        {
+            winText.text = "You win!";
+        }
     }
 }
